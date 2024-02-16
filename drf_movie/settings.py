dac6f0c97754d472d1dd87b2465b77adf6e49349
Eduard Latypov <LatypovEdik1988@yandex.ru>
2024-02-16 16:27:06 +0300

    Установил django-cors-headers

--- drf_movie/settings.py
+++ drf_movie/settings.py
@@ -53,12 +53,14 @@ INSTALLED_APPS = [
     "drf_yasg",
     "debug_toolbar",
     "django_filters",
+    "corsheaders",
     "movies",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -201,6 +203,12 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+]
+
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
